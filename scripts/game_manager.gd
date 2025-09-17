696a337adae2809dff27fed2cda65feda3094792
Marcos dos Santos <rrmarcosdossantos@gmail.com>
2025-09-17 08:24:30 -0300

    Adjust start and go features

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,9 +6,6 @@ extends Node
 
 var current_room: Room
 var is_game_started: bool = false
-#var current_specting_stuff: Stuff
-var turns: int = 10
-
 
 const INTRO_MESSAGE: String = "[b][font_size=32]Welcome to Escape in 10 Turns![/font_size][/b]\nThis is a text-based RPG where every choice matters. Your mission is simple: escape the scenario in fewer than 10 turns. Each action you take will cost you 1 turn, so plan carefully.\n\nYou can choose from the following actions: Go, Inspect, Get, Inventory and Use.\n\nWhen you’re ready, type Start to begin your adventure."
 
@@ -18,13 +15,17 @@ func _on_text_input_entered(text: String) -> void:
 	execute_action(command)
 	
 func execute_action(command: Dictionary[String, String]):
-	match command["action"]:
-		"start":
+	if is_game_started:
+		match command["action"]:
+			"go":
+				go(command["complement"])
+			_:
+				text_interface.update_narrative("Invalid action") 
+	else:
+		if command["action"] == "start":
 			start_game()
-		"go":
-			go(command["complement"])
-		_:
-			print("ação inválida")
+		else:
+			text_interface.update_narrative("Invalid action") 
 	
 func start_game():
 	is_game_started = true
@@ -33,31 +34,14 @@ func start_game():
 	
 func go(location: String) -> void:
 	for exit in current_room.exits:
-		if location.to_lower() == exit.title.to_lower():
-			text_interface.update_narrative("Go " + location)
+		if location == exit.title.to_lower():
+			text_interface.update_narrative("Go " + exit.title)
 			current_room = exit
-			current_room.inspect()
+			text_interface.update_narrative(current_room.inspect()) 
 			return
 
 func _ready() -> void:
 	text_interface.input_text_entered.connect(_on_text_input_entered)
 	text_interface.update_narrative(INTRO_MESSAGE)
 	current_room = starting_room
-
-#func show_current_room_description() -> void:
-	#update_narrative(current_room.description) 
-
-
-#func start_action() -> void:
-	#is_game_started = true
-	#reset_narrative()
-	#reset_user_input()
-	#show_current_room_description()
-	
-
-#
-	#update_narrative("[color=" + orange_color + "]Go[/color] " + location)
-	#update_narrative(INVALID_COMPLEMENT_MESSAGE + location)
-
-
 	
