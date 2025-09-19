237dd5203a1a35ea143a48f0a52ec6b961039c63
Marcos dos Santos <rrmarcosdossantos@gmail.com>
2025-09-19 18:58:02 -0300

    add new player class and show_inventory feature

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,9 +3,11 @@ extends Node
 @export var text_interface: TextInterface
 @export var command_parser: CommandParser
 @export var starting_room: Room
+@export var player: Player
 
 var current_room: Room
 var is_game_started: bool = false
+var is_inventory_open: bool = false
 
 const INTRO_MESSAGE: String = "[b][font_size=32]Welcome to Escape in 10 Turns![/font_size][/b]\nThis is a text-based RPG where every choice matters. Your mission is simple: escape the scenario in fewer than 10 turns. Each action you take will cost you 1 turn, so plan carefully.\n\nYou can choose from the following actions: Go, Inspect, Get, Inventory and Use.\n\nWhen you’re ready, type Start to begin your adventure."
 
@@ -18,6 +20,9 @@ func execute_action(command: Dictionary[String, String]):
 	text_interface.update_narrative(command["raw_text"])
 	
 	if is_game_started:
+		if command["action"] == "inventory" && not command["complement"]:
+			text_interface.update_narrative(player.show_inventory()) 
+			return
 		match command["action"]:
 			"go":
 				go(command["complement"])
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,27 @@
 extends Node
 
+class_name Player
+
 var inventory: Array[Item] = []
 
-func show_inventory() -> void:
+func show_inventory() -> String:
 	if inventory:
-		var items_list: String = ""
+		var inventory_items: String
 		for item in inventory:
-			items_list += "- " + item.title + "\n"
-		update_narrative("Your inventory contains the following items:\n" + items_list)
+			inventory_items += "- " + item.title + "\n"
+		return "Your inventory[/color] contains the following items:\n" + inventory_items
 	else:
-		update_narrative("Your inventory is empty.")
+		return "Your inventory is empty."
 
-func take_item(target: String) -> void:
-	if current_specting_stuff:
-		for item in current_specting_stuff.items:
-			if target.to_lower() == item.title.to_lower():
-				update_narrative("[color=" + pink_color + "]Get[/color] " + target)
-				update_narrative("You add a " + item.title + " to your inventory.")
-				current_specting_stuff.items.erase(item)
-				inventory.append(item)
-				return
-				
-	update_narrative("[color=" + pink_color + "]Get[/color] " + target)
-	update_narrative(INVALID_COMPLEMENT_MESSAGE + target)
+#func take_item(target: String) -> void:
+	#if current_specting_stuff:
+		#for item in current_specting_stuff.items:
+			#if target.to_lower() == item.title.to_lower():
+				#update_narrative("[color=" + pink_color + "]Get[/color] " + target)
+				#update_narrative("You add a " + item.title + " to your inventory.")
+				#current_specting_stuff.items.erase(item)
+				#inventory.append(item)
+				#return
+				#
+	#update_narrative("[color=" + pink_color + "]Get[/color] " + target)
+	#update_narrative(INVALID_COMPLEMENT_MESSAGE + target)
